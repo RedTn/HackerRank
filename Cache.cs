c2c42860db9673952263141474ee26e6cbd31930
RedTn <redmond.tran@gmail.com>
2017-04-05 18:05:35 -0700

    cache implemented

--- Cache.cs
+++ Cache.cs
@@ -11,6 +11,7 @@ namespace Solution
         public class LinkedListNode
         {
             public TypeValue Value { get; set; }
+            public TypeKey Key { get; set; }
             public LinkedListNode Next { get; set; }
             public LinkedListNode Previous { get; set; }
         }
@@ -25,23 +26,92 @@ namespace Solution
             Capacity = capacity;
         }
 
-        public bool TryGetValue(TypeKey key, out TypeValue value)
+        public bool TryGetValue(TypeKey key, out TypeValue val)
         {
-            value = default(TypeValue);
+            val = default(TypeValue);
             LinkedListNode entry;
             if (!map.TryGetValue(key, out entry)) return false;
             MoveToHead(entry);
-            value = entry.Value;
+            val = entry.Value;
             return true;
+        }
+
+        public void Set(TypeKey key, TypeValue val)
+        {
+            LinkedListNode node = new LinkedListNode();
+            if (map.TryGetValue(key, out node))
+            {
+                MoveToHead(node);
+                return;
+            }
+            node = new LinkedListNode();
+            if (map.Count() >= Capacity)
+            {
+                map.Remove(tail.Key);
+                LinkedListNode previous = tail.Previous;
+                tail = previous;
+                previous.Next = null;
+            }
+            node.Value = val;
+            node.Key = key;
+            MoveToHead(node);
+            map.Add(key, node);
         }
 
         public void MoveToHead(LinkedListNode node)
         {
-            if (head == null || head == node)
+            if (head == null)
             {
-                tail = head;
+                head = tail = node;
                 return;
             }
+            if (head == node)
+            {
+                return;
+            }
+
+            LinkedListNode prev = node.Previous;
+            LinkedListNode next = node.Next;
+
+
+            if (prev != null)
+            {
+                if (next == null)
+                {
+                    prev.Next = null;
+                    tail = prev;
+                }
+                else
+                {
+                    prev.Next = next;
+                    next.Previous = prev;
+                }
+            }
+
+            node.Next = head;
+            node.Next.Previous = node;
+            head = node;
+            head.Previous = null;
+        }
+
+        public void Debug()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Hash output:");
+            foreach (var m in map)
+            {
+                sb.AppendFormat("Key: {0}, Value: {1}", m.Key.ToString(), m.Value.Value.ToString());
+                sb.AppendLine();
+            }
+            sb.AppendLine("Queue output:");
+            LinkedListNode node = head;
+            while (node != null)
+            {
+                sb.AppendFormat("Node: {0}", node.Value);
+                sb.AppendLine();
+                node = node.Next;
+            }
+            Console.WriteLine(sb.ToString());
         }
     }
 }
